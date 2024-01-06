08b74d33939acab61c1ecc090b3516f56415b0f2
task4233 <29667656+task4233@users.noreply.github.com>
2024-01-06 14:17:53 +0900

    change(ch3/ex1): add implementation for authorization_code grant

--- exercises/ch-3-ex-1/client.js
+++ exercises/ch-3-ex-1/client.js
@@ -27,8 +27,8 @@ var authServer = {
  * Add the client information in here
  */
 var client = {
-	"client_id": "",
-	"client_secret": "",
+	"client_id": "oauth-client-1",
+	"client_secret": "oauth-client-secret-1",
 	"redirect_uris": ["http://localhost:9000/callback"]
 };
 
@@ -40,58 +40,85 @@ var access_token = null;
 var scope = null;
 
 app.get('/', function (req, res) {
-	res.render('index', {access_token: access_token, scope: scope});
+	res.render('index', { access_token: access_token, scope: scope });
 });
 
-app.get('/authorize', function(req, res){
+app.get('/authorize', function (req, res) {
 
 	/*
 	 * Send the user to the authorization server
 	 */
-	
+	const authorizeUrl = buildUrl(authServer.authorizationEndpoint, {
+		response_type: 'code',
+		client_id: client.client_id,
+		redirect_uri: client.redirect_uris[0]
+	});
+
+	res.redirect(authorizeUrl);
 });
 
-app.get('/callback', function(req, res){
+app.get('/callback', function (req, res) {
 
 	/*
 	 * Parse the response from the authorization server and get a token
 	 */
-	
+	const code = req.query.code;
+
+	const form_data = qs.stringify({
+		grant_type: 'authorization_code',
+		code: code,
+		redirect_uri: client.redirect_uris[0],
+	})
+
+	const headers = {
+		'Content-Type': 'application/x-www-form-urlencoded',
+		'Authorization': `Basic ${encodeClientCredentials(client.client_id, client.client_secret)}`
+	}
+
+	const tokenRes = request('POST', authServer.tokenEndpoint,
+		{
+			body: form_data,
+			headers: headers
+		}
+	);
+	const body = JSON.parse(tokenRes.getBody());
+	res.render('index', { access_token: body.access_token, scope: body.scope});
 });
 
-app.get('/fetch_resource', function(req, res) {
+app.get('/fetch_resource', function (req, res) {
 
 	/*
 	 * Use the access token to call the resource server
 	 */
-	
+
 });
 
-var buildUrl = function(base, options, hash) {
+var buildUrl = function (base, options, hash) {
 	var newUrl = url.parse(base, true);
 	delete newUrl.search;
 	if (!newUrl.query) {
 		newUrl.query = {};
 	}
-	__.each(options, function(value, key, list) {
+	// 
+	__.each(options, function (value, key, list) {
 		newUrl.query[key] = value;
 	});
 	if (hash) {
 		newUrl.hash = hash;
 	}
-	
+
 	return url.format(newUrl);
 };
 
-var encodeClientCredentials = function(clientId, clientSecret) {
+var encodeClientCredentials = function (clientId, clientSecret) {
 	return Buffer.from(querystring.escape(clientId) + ':' + querystring.escape(clientSecret)).toString('base64');
 };
 
 app.use('/', express.static('files/client'));
 
 var server = app.listen(9000, 'localhost', function () {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('OAuth Client is listening at http://%s:%s', host, port);
+	var host = server.address().address;
+	var port = server.address().port;
+	console.log('OAuth Client is listening at http://%s:%s', host, port);
 });
- 
+
